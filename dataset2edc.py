b63bd3dd931c4182d6b42605b7c728c7735a8edc
Aethor <oow.autre@gmail.com>
2025-05-15 12:30:20 +0800

    Add dataset2edc.py

---
+++ dataset2edc.py
@@ -0,0 +1,111 @@
+# This script converts a generated text-to-quadruple dataset into the
+# format expected by EDC. More specifically:
+#
+# The input dataset is a list of dictionaries, each representing a
+# text/quadruple pair. Each dict has the following entries:
+#
+# - "subject"     : the subject of the predictate
+# - "relation"    : the relation between subject/object
+# - "object"      : the object of the relation
+# - "timestamp"   : timestamp of the relation, in YYYY-MM-DD format
+# - "description" : description of the quadruple. The description
+#                   should be used to recover the quadruple.
+#
+# The script outputs is as follows:
+#
+# - ./dsets/{dataset_name}.json : quadruple description, one per line
+# - ./evaluate/references/{dataset_name}.json :
+from typing import Tuple
+import argparse, json, re
+import pathlib as pl
+
+Quad = Tuple[str, str, str, str]
+
+
+def string_lstrip(s: str, to_strip: str) -> str:
+    try:
+        s = s[s.index(to_strip) + len(to_strip) :]
+    except ValueError:
+        pass
+    return s
+
+
+def clean_prefix(elt: str) -> str:
+    elt = string_lstrip(elt, "yago:")
+    elt = string_lstrip(elt, "schema:")
+    return elt
+
+
+def clean_quad_prefix(quad: Quad) -> Quad:
+    subj, rel, obj, ts = quad
+    return (clean_prefix(subj), clean_prefix(rel), clean_prefix(obj), clean_prefix(ts))
+
+
+def parse_hex_unicode(hex_unicode: str) -> str:
+    assert hex_unicode.startswith("u")
+    return chr(int(hex_unicode[1:], base=16))
+
+
+def clean_unicode(elt: str) -> str:
+    return re.sub(r"_u[0-9A-E]{4}", lambda m: parse_hex_unicode(m.group()[1:]), elt)
+
+
+def clean_quad_unicode(quad: Quad) -> Quad:
+    subj, rel, obj, ts = quad
+    return (clean_unicode(subj), rel, clean_unicode(obj), ts)
+
+
+def clean_underscore(elt: str) -> str:
+    elt = re.sub(r"_$", "", elt)
+    elt = re.sub(r"_+", " ", elt)
+    return elt
+
+
+def clean_quad_underscore(quad: Quad) -> Quad:
+    subj, rel, obj, ts = quad
+    return (clean_underscore(subj), rel, clean_underscore(obj), ts)
+
+
+def clean_wiki_id(elt: str) -> str:
+    return re.sub(r"Q[0-9]+", "", elt)
+
+
+def clean_quad_wiki_id(quad: Quad) -> Quad:
+    subj, rel, obj, ts = quad
+    return (clean_wiki_id(subj), rel, clean_wiki_id(obj), ts)
+
+
+def format_quad(quad: Quad) -> Quad:
+    quad = clean_quad_prefix(quad)
+    quad = clean_quad_unicode(quad)
+    quad = clean_quad_wiki_id(quad)
+    quad = clean_quad_underscore(quad)
+    return quad
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-file", type=pl.Path)
+    args = parser.parse_args()
+
+    with open(args.input_file) as f:
+        data = json.load(f)
+
+    desc_path = pl.Path("./dsets/") / f"{args.input_file.stem}.txt"
+    print(f"writing to {desc_path}...", end="")
+    with open(desc_path, "w") as f:
+        for quad in data:
+            f.write(f"{quad['description']}\n")
+    print("done!")
+
+    ref_path = pl.Path("./evaluate/references/") / f"{args.input_file.stem}.txt"
+    print(f"writing to {ref_path}...", end="")
+    with open(ref_path, "w") as f:
+        for quad in data:
+            quad = format_quad(
+                (quad["subject"], quad["relation"], quad["object"], quad["timestamp"])
+            )
+            subj, rel, obj, ts = quad
+            f.write(f"[['{subj}', '{rel}', '{obj}', '{ts}']]\n")
+    print("done!")
