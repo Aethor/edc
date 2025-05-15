4fb265472cc7d8cec5ad9a6c86ec6e62b16fd157
Aethor <oow.autre@gmail.com>
2025-05-15 12:30:07 +0800

    add tests for llm_utils

---
+++ tests/test_llm_utils.py
@@ -0,0 +1,20 @@
+from typing import List
+from hypothesis import given, strategies as st
+from string import ascii_letters
+from edc.utils.llm_utils import parse_raw_quadruples, parse_raw_entities
+
+
+@given(st.lists(st.text(alphabet=ascii_letters, min_size=1), min_size=4, max_size=4))
+def test_parse_trivial_quadruples(quad: List[str]):
+    assert len(quad) == 4
+    parsed = parse_raw_quadruples(str(quad))
+    assert parsed.__class__ == list
+    assert len(parsed) == 1  # only 1 quadruple
+    assert len(parsed[0]) == 4  # the quadruple has 4 elements
+
+
+@given(st.lists(st.text(alphabet=ascii_letters)))
+def test_parse_raw_entities(raw_entities_lst: List[str]):
+    parsed = parse_raw_entities(str(raw_entities_lst))
+    assert parsed.__class__ == list
+    assert len(parsed) == len(raw_entities_lst)
