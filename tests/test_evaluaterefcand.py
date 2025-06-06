ceba7a6f86388ede67c312640f0e0ffa26484d04
aethor <oow.autre@gmail.com>
2025-06-06 10:30:15 +0800

    improve testing for evaluaterefcand

--- tests/test_evaluaterefcand.py
+++ tests/test_evaluaterefcand.py
@@ -5,21 +5,39 @@ from evaluate.archive import evaluaterefcand as evaluaterefcand_old
 from evaluate.evaluation_script import evaluaterefcand
 
 
-@given(
-    st.text(alphabet=ascii_letters, min_size=1),
-    st.text(alphabet=ascii_letters, min_size=1),
-    st.text(alphabet=ascii_letters, min_size=1),
-    st.text(alphabet=ascii_letters, min_size=1),
-    st.text(alphabet=ascii_letters, min_size=1),
-    st.text(alphabet=ascii_letters, min_size=1),
-)
-def test_triplet_same_output(
-    refsub: str, refpred: str, refobj: str, predsub: str, predpred: str, predobj: str
-):
-    ref = f"{refsub} | {refpred} | {refobj}"
-    pred = f"{predsub} | {predpred} | {predobj}"
+@st.composite
+def st_quad(draw, **kwargs) -> str:
+    sub = draw(st.text(alphabet=ascii_letters, **kwargs))
+    pred = draw(st.text(alphabet=ascii_letters, **kwargs))
+    obj = draw(st.text(alphabet=ascii_letters, **kwargs))
+    ts = draw(st.text(alphabet=ascii_letters, **kwargs))
+    return f"{sub} | {pred} | {obj} | {ts}"
+
+
+@st.composite
+def st_triple(draw, **kwargs) -> str:
+    sub = draw(st.text(alphabet=ascii_letters, **kwargs))
+    pred = draw(st.text(alphabet=ascii_letters, **kwargs))
+    obj = draw(st.text(alphabet=ascii_letters, **kwargs))
+    return f"{sub} | {pred} | {obj}"
+
+
+@given(st_triple(min_size=1), st_triple(min_size=1))
+def test_retrocompatible(ref: str, cand: str):
     with open(os.devnull, "w") as devnull:
         with contextlib.redirect_stdout(devnull):
-            old_out = evaluaterefcand_old(ref, pred)
-            new_out = evaluaterefcand(ref, pred)
+            old_out = evaluaterefcand_old(ref, cand)
+            new_out = evaluaterefcand(ref, cand)
     assert old_out == new_out
+
+
+# @given(st_quad())
+# def test_identical_output_has_perfect_score(quad: str):
+#     results, result_per_tag = evaluaterefcand(quad, quad)
+#     for metric_variant in ["ent_type", "partial", "strict", "exact"]:
+#         assert results[metric_variant]["f1"] == 1.0
+
+
+# @given(st_quad(), st_quad())
+# def test_quad(ref: str, cand: str):
+#     assert evaluaterefcand(ref, cand)
