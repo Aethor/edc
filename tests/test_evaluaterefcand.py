65e98dec0472aaade678006a04ef9a3a5284429d
aethor <oow.autre@gmail.com>
2025-06-11 18:44:43 +0800

    improve evaluaterefcand testing

--- tests/test_evaluaterefcand.py
+++ tests/test_evaluaterefcand.py
@@ -1,43 +1,72 @@
-import os, contextlib
-from hypothesis import given, strategies as st
+from hypothesis import assume, example, given, strategies as st
 from string import ascii_letters
-from evaluate.archive import evaluaterefcand as evaluaterefcand_old
+from datetime import datetime, timedelta
 from evaluate.evaluation_script import evaluaterefcand
 
 
 @st.composite
-def st_quad(draw, **kwargs) -> str:
-    sub = draw(st.text(alphabet=ascii_letters, **kwargs))
-    pred = draw(st.text(alphabet=ascii_letters, **kwargs))
-    obj = draw(st.text(alphabet=ascii_letters, **kwargs))
-    ts = draw(st.text(alphabet=ascii_letters, **kwargs))
-    return f"{sub} | {pred} | {obj} | {ts}"
+def st_timestamps(draw, start: datetime, end: datetime) -> str:
+    delta = end - start
+    out_date = start + timedelta(
+        days=draw(st.integers(min_value=0, max_value=delta.days))
+    )
+    return out_date.strftime("%Y-%m-%d")
 
 
 @st.composite
-def st_triple(draw, **kwargs) -> str:
+def st_quads(draw, **kwargs) -> str:
     sub = draw(st.text(alphabet=ascii_letters, **kwargs))
     pred = draw(st.text(alphabet=ascii_letters, **kwargs))
     obj = draw(st.text(alphabet=ascii_letters, **kwargs))
-    return f"{sub} | {pred} | {obj}"
+    ts = draw(st_timestamps(datetime(1900, 1, 1), datetime(2050, 1, 1)))
+    return f"{sub} | {pred} | {obj} | {ts}"
 
 
-@given(st_triple(min_size=1), st_triple(min_size=1))
-def test_retrocompatible(ref: str, cand: str):
-    with open(os.devnull, "w") as devnull:
-        with contextlib.redirect_stdout(devnull):
-            old_out = evaluaterefcand_old(ref, cand)
-            new_out = evaluaterefcand(ref, cand)
-    assert old_out == new_out
+@given(st_quads(min_size=1))
+def test_perfect_cand_gives_perfect_score(ref: str):
+    results, _ = evaluaterefcand(ref, ref)
+    for metric_mode in ["strict", "exact", "partial", "ent_type"]:
+        assert results[metric_mode]["precision"] == 1.0
+        assert results[metric_mode]["recall"] == 1.0
+        assert results[metric_mode]["f1"] == 1.0
 
 
-# @given(st_quad())
-# def test_identical_output_has_perfect_score(quad: str):
-#     results, result_per_tag = evaluaterefcand(quad, quad)
-#     for metric_variant in ["ent_type", "partial", "strict", "exact"]:
-#         assert results[metric_variant]["f1"] == 1.0
+def swap_attrs(quad: str, i: int, j: int) -> str:
+    swapped = quad.split(" | ")
+    tmp = swapped[i]
+    swapped[i] = swapped[j]
+    swapped[j] = tmp
+    swapped = " | ".join(swapped)
+    return swapped
 
 
-# @given(st_quad(), st_quad())
-# def test_quad(ref: str, cand: str):
-#     assert evaluaterefcand(ref, cand)
+@given(
+    st_quads(min_size=1),
+    st.integers(min_value=0, max_value=3),
+    st.integers(min_value=0, max_value=3),
+)
+def test_swap_perfect_cand_attrs_gives_perfect_exact_and_partial_score(
+    ref: str, i: int, j: int
+):
+    assume(i != j)
+    cand = swap_attrs(ref, i, j)
+    results, _ = evaluaterefcand(ref, cand)
+    assert results["exact"]["f1"] == 1.0
+    assert results["partial"]["f1"] == 1.0
+
+
+@given(st_quads(min_size=1))
+def test_unaligned_elements_gives_null_strict_and_type_score(ref: str):
+    assume(len(set(ref.split(" | "))) == 4)
+    cand = swap_attrs(ref, 0, 2)
+    cand = swap_attrs(cand, 1, 3)
+    results, _ = evaluaterefcand(ref, cand)
+    assert results["ent_type"]["f1"] == 0.0
+    assert results["strict"]["f1"] == 0.0
+
+
+@given(st_quads(min_size=1), st_quads(min_size=1))
+def test_score_ordering_is_correct(ref: str, cand: str):
+    results, _ = evaluaterefcand(ref, cand)
+    assert results["ent_type"]["f1"] >= results["strict"]["f1"]
+    assert results["exact"]["f1"] >= results["strict"]["f1"]
