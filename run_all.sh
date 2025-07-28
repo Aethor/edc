44217e23f23c9f5d5836e47ae31b34e4a965f360
aethor <oow.autre@gmail.com>
2025-07-28 15:29:58 +0800

    use static datasets instead of dynamically generating them

--- run_all.sh
+++ run_all.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# this creates:
-# - yago2022:balanced-yago2026
-# - yago2026:balanced-yago2022
-python balanced_datasets.py\
-       --first-dataset "yago2022"\
-       --second-dataset "yago2026"
-
-# this creates:
-# - yago2022_multi:balanced-yago2026_multi
-# - yago2026_multi:balanced-yago2022_multi
-python balanced_datasets.py\
-       --first-dataset "yago2022_multi"\
-       --second-dataset "yago2026_multi"
-
 for dataset in 'yago2022:balanced-yago2026' 'yago2026:balanced-yago2022' 'yago2022_multi:balanced-yago2026_multi' 'yago2026_multi:balanced-yago2022_multi'; do
 
     python run.py \
@@ -29,31 +15,6 @@ for dataset in 'yago2022:balanced-yago2026' 'yago2026:balanced-yago2022' 'yago20
         --output_dir "./output/${dataset}_target_alignment"
 
 done
-
-
-python retimestamped_dataset.py\
-       --input-dataset "yago2022:balanced-yago2026"\
-       --output-dataset "yago2022:balanced-yago2026:retimestamped-2026"\
-       --old-year 2022\
-       --new-year 2026
-
-python retimestamped_dataset.py\
-       --input-dataset "yago2026:balanced-yago2022"\
-       --output-dataset "yago2026:balanced-yago2022:retimestamped-2022"\
-       --old-year 2026\
-       --new-year 2022
-
-python retimestamped_dataset.py\
-       --input-dataset "yago2022_multi:balanced-yago2026_multi"\
-       --output-dataset "yago2022_multi:balanced-yago2026_multi:retimestamped-2026"\
-       --old-year 2022\
-       --new-year 2026
-
-python retimestamped_dataset.py\
-       --input-dataset "yago2026_multi:balanced-yago2022_multi"\
-       --output-dataset "yago2026_multi:balanced-yago2022_multi:retimestamped-2022"\
-       --old-year 2026\
-       --new-year 2022
 
 for dataset in 'yago2022:balanced-yago2026:retimestamped-2026' 'yago2026:balanced-yago2022:retimestamped-2022' 'yago2022_multi:balanced-yago2026_multi:retimestamped-2026' 'yago2026_multi:balanced-yago2022_multi:retimestamped-2022'; do
 
