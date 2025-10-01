4a57597242e70f4c82b8e24eaa62130dfab65108
aethor <oow.autre@gmail.com>
2025-10-01 17:08:41 +0800

    correctly create output directory in run_baseline.py

--- run_baseline.py
+++ run_baseline.py
@@ -1,4 +1,4 @@
-import csv
+import csv, os
 from argparse import ArgumentParser
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from edc.extract import Extractor
@@ -57,6 +57,7 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(args.llm)
     extractor = Extractor(model, tokenizer)
 
+    os.makedirs(f"{args.output_dir}/iter0", exist_ok=True)
     with open(f"{args.output_dir}/iter0/canon_kg.txt", "w") as f:
         for i, text in enumerate(input_text_list):
             quads = extractor.extract(
