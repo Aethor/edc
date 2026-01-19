d593b2432dfa8cadc5a0f9f2a76258d43360ab89
aethor <oow.autre@gmail.com>
2026-01-19 10:49:47 +0800

    fix llama3.1 exact model name in score_baseline script

--- score_baseline.sh
+++ score_baseline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-models=('mistralai/Mistral-7B-Instruct-v0.2' 'meta-llama/Llama-3.1-8B')
+models=('mistralai/Mistral-7B-Instruct-v0.2' 'meta-llama/Llama-3.1-8B-Instruct')
 
 datasets=('yago2022:balanced-yago2026' 'yago2026:balanced-yago2022' 'yago2022_multi:balanced-yago2026_multi' 'yago2026_multi:balanced-yago2022_multi' 'yago2022:balanced-yago2026:retimestamped-2026' 'yago2026:balanced-yago2022:retimestamped-2022' 'yago2022_multi:balanced-yago2026_multi:retimestamped-2026' 'yago2026_multi:balanced-yago2022_multi:retimestamped-2022')
 
