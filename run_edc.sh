67d310df774cf68fd10de9fd4460a292503ca10d
aethor <oow.autre@gmail.com>
2026-03-25 10:11:23 +0800

    update run/score scripts with new models, updated datasets

--- run_edc.sh
+++ run_edc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-datasets=('yago2022:balanced-yago2026' 'yago2026:balanced-yago2022' 'yago2022_multi:balanced-yago2026_multi' 'yago2026_multi:balanced-yago2022_multi' 'yago2022:balanced-yago2026:retimestamped-2026' 'yago2026:balanced-yago2022:retimestamped-2022' 'yago2022_multi:balanced-yago2026_multi:retimestamped-2026' 'yago2026_multi:balanced-yago2022_multi:retimestamped-2022')
+models=('hf:mistralai/Mistral-7B-Instruct-v0.2' 'hf:meta-llama/Llama-3.1-8B-Instruct')
 
-model='hf:mistralai/Mistral-7B-Instruct-v0.2'
+
+datasets=('yago2019:balanced-yago2026' 'yago2026:balanced-yago2019' 'yago2019_multi:balanced-yago2026_multi' 'yago2026_multi:balanced-yago2019_multi' 'yago2019:balanced-yago2026:retimestamped-2026' 'yago2026:balanced-yago2019:retimestamped-2019' 'yago2019_multi:balanced-yago2026_multi:retimestamped-2026' 'yago2026_multi:balanced-yago2019_multi:retimestamped-2019')
 
 for dataset in "${datasets[@]}"; do
 
